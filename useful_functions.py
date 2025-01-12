3f85f29f78dba4a18010dcd5e1e25b9c72b98611
Dominic Bates <thisisdom23@googlemail.com>
2025-01-12 14:49:52 +0000

    Added some nicer plotting functions to plot all marginals easily

--- useful_functions.py
+++ useful_functions.py
@@ -64,3 +64,88 @@ def marginals_hist(vals,
     
     plt.legend()
     plt.title(feature_name)
+
+
+
+
+
+
+def plot_all_marginals(trace,
+                       parameters=None,
+                       parameter_names = None,
+                       grid_shape = (2,2), 
+                       figsize = None):
+    '''
+    Function that accepts a trace, and plots all marginals nicely.
+
+    Args:
+    - parameters: A list of paramters in `trace` to plot (e.g. ['betas', 'intercept']
+    - parameter_names: A dict of paramter names or sub-names for adding to the plot E.g.
+        {'betas':['f1', 'f2', 'f3'], 
+         'intercept': 'intercept'}
+    - grid_shape: A tuple containing the number of rows and columns for the plot
+    '''
+
+    # Set if args are None
+    if parameters == None:
+        parameters = list(trace.posterior)
+    rows, cols = grid_shape
+    if figsize == None:
+        figsize = (cols * 3.5, rows * 2.75)
+        
+        
+    # Loop through parameters to count n. plots
+    tot = 0
+    for param in parameters:
+        # Count parameters
+        if len(trace.posterior[param].shape) == 3:
+            tot += trace.posterior[param].shape[-1] # If several sub-parameters, add all
+        else:
+            tot += 1 # Otherwise just add 1 parameter
+    
+    # Check if fits in grid
+    if tot > (grid_shape[0]*grid_shape[1]):
+        raise ValueError(f'Total number of parameters to plot (n = {tot}) exceeds grid size ({grid_shape[0]} * {grid_shape[1]}). Increase grid size to fit all parameter plots or pick fewer parameters ({str(parameters)})')
+    
+    # Create plot
+    fig, axes = plt.subplots(rows, cols, figsize=figsize)  # Adjust figure size as needed
+    
+    # Loop through parameters and plot
+    count = 0
+    color_count = 0
+    for param in parameters:
+        
+        # If parameter has several sub-parameters
+        if len(trace.posterior[param].shape) == 3:
+            param_count = 0
+            for n in range(trace.posterior[param].shape[-1]):
+                # Extract values and feature name
+                vals = trace.posterior[param].values[:,:,n].flatten()
+                try:
+                    f_name = parameter_names[param][param_count]
+                except:
+                    f_name = param + '_' + str(param_count+1)
+    
+                # Make plot
+                plt.subplot(rows, cols, count+1)
+                marginals_hist(vals, f_name, create_new_fig=False, color='C'+str(color_count))
+                count += 1; param_count += 1
+        else:
+            # Extract values and feature name
+            vals = trace.posterior[param].values.flatten()
+            try:
+                f_name = parameter_names[param]
+            except:
+                f_name = str(param)
+    
+            # Make plot
+            plt.subplot(rows, cols, count+1)
+            marginals_hist(vals, f_name, create_new_fig=False, color='C'+str(color_count))
+            count += 1 
+    
+        # Go to next colour
+        color_count+=1
+    
+    # Adjust layout
+    plt.tight_layout()
+
